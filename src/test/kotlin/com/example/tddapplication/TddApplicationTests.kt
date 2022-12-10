936d31066233531ff969b72baf9d660a825be888
CunningBird <68229355+CunningBird@users.noreply.github.com>
2022-12-10 14:09:34 +0300

    initial commit

---
+++ src/test/kotlin/com/example/tddapplication/TddApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tddapplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TddApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
